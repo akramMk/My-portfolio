27e1a2320261079d7caa8f652275841fc9c39ba9
akramMk <akrammekbal10@gmail.com>
2025-04-13 21:29:01 +0200

    add service Component

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -2,6 +2,7 @@
   <div class="bg-[#111827] min-h-screen">
     <NavBar></NavBar>
     <HeroSection></HeroSection>
+    <ServicesSection></ServicesSection>
   </div>
 </template>
 
@@ -10,7 +11,7 @@ import { defineAsyncComponent } from 'vue';
 
 const NavBar = defineAsyncComponent(()=>import('@/components/NavBar.vue'))
 const HeroSection = defineAsyncComponent(()=>import('@/components/HeroSection.vue'))
-
+const ServicesSection = defineAsyncComponent(()=>import('@/components/ServicesSection.vue'))
 </script>
 
 <style scoped>
